e1749ecbd15503c5e3d8b0ff9c8ccadab2c2b07b
jgee <jacobgee87@gmail.com>
2019-07-25 14:50:41 -0500

    working Sword Coast map, input first marker: Neverwinter (no details on marker)

--- client/components/map.js
+++ client/components/map.js
@@ -3,14 +3,16 @@ import L from 'leaflet'
 
 class LeafletMap extends React.Component {
   componentDidMount() {
-    this.map = L.map('mapContainer').setView([51.505, -0.09], 13)
-    var layer = L.tileLayer(
-      'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
-      {
-        attribution:
-          '&copy; <a href="http://www.openstreetmap.org/copyright"> OpenStreetMap</a> contributors, &copy; <ahref="https://carto.com/attributions">CARTO</a>'
-      }
+    this.map = L.map('mapContainer', {crs: L.CRS.Simple, minZoom: -5})
+    var bounds = [[0, 0], [660, 1020]]
+    L.imageOverlay(
+      'https://media.wizards.com/2015/images/dnd/resources/Sword-Coast-Map_HighRes.jpg',
+      bounds
     ).addTo(this.map)
+    this.map.fitBounds(bounds)
+
+    var neverwinter = L.latLng([524, 406.3])
+    L.marker(neverwinter).addTo(this.map)
   }
   render() {
     return (
