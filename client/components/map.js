42758bac0cd8d64b4a37c591406e5b0cea788c15
jgee <jacobgee87@gmail.com>
2019-07-26 10:38:29 -0500

    added icons

--- client/components/map.js
+++ client/components/map.js
@@ -6,21 +6,53 @@ class LeafletMap extends React.Component {
     this.map = L.map('mapContainer', {crs: L.CRS.Simple, minZoom: -5})
     var bounds = [[0, 0], [660, 1020]]
     L.imageOverlay(
-      'https://media.wizards.com/2015/images/dnd/resources/Sword-Coast-Map_HighRes.jpg',
+      'https://media.wizards.com/2015/images/dnd/resources/Sword-Coast-Map_MedRes.jpg',
       bounds
     ).addTo(this.map)
     this.map.fitBounds(bounds)
 
-    var neverwinter = L.marker(L.latLng([524, 406.3])).addTo(this.map)
-    neverwinter.bindPopup(
-      '<a href="https://forgottenrealms.fandom.com/wiki/Neverwinter" target="_blank"><b>Neverwinter</b></a>'
-    )
+    //icons
 
-    var trollbarkForest = L.marker(L.latLng([330, 510])).addTo(this.map)
-    trollbarkForest.bindPopup(
-      '<a href="https://forgottenrealms.fandom.com/wiki/Trollbark_Forest" target="_blank"><b>Trollbark Forest</b></a>'
+    //markers
+    var neverwinter = L.marker([524, 406.3]).addTo(this.map)
+    neverwinter.bindPopup(
+      '<a href="https://forgottenrealms.fandom.com/wiki/Neverwinter" target="_blank"><b>More Info</b></a>'
     )
+    neverwinter.bindTooltip('Neverwinter')
+
+    var helmsHold = L.marker([518, 417.5]).addTo(this.map)
+    helmsHold.bindPopup(
+      '<a href="https://forgottenrealms.fandom.com/wiki/Helm%27s_Hold" target="_blank"><b>More Info</b></a>'
+    )
+    helmsHold.bindTooltip("Helm's Hold")
+
+    var gauntlgrym = L.marker([547.8, 418.5]).addTo(this.map)
+    gauntlgrym.bindPopup(
+      '<a href="https://forgottenrealms.fandom.com/wiki/Gauntlgrym" target="_blank"><b>More Info</b></a>'
+    )
+    gauntlgrym.bindTooltip('Gauntlgrym')
+
+    var trollbarkForest = L.marker([330, 510]).addTo(this.map)
+    trollbarkForest.bindPopup(
+      '<a href="https://forgottenrealms.fandom.com/wiki/Trollbark_Forest" target="_blank"><b>More Info</b></a>'
+    )
+    trollbarkForest.bindTooltip('Trollbark Forest')
+
+    var highIce = L.marker([565, 780]).addTo(this.map)
+    highIce.bindPopup(
+      '<a href="https://forgottenrealms.fandom.com/wiki/High_Ice" target="_blank"><b>More Info</b></a>'
+    )
+    highIce.bindTooltip('High Ice')
+
+    var tracklessSea = L.marker([470, 170]).addTo(this.map)
+    tracklessSea.bindPopup(
+      '<a href="https://forgottenrealms.fandom.com/wiki/Trackless_Sea" target="_blank"><b>More Info</b></a>'
+    )
+    tracklessSea.bindTooltip('Trackless Sea')
   }
+
+  //Layer Groups
+
   render() {
     return (
       <div className="components">
